5486aacd71d2ec0ac228a01439c753974e7a84ef
jimmyale3102 <jimmyale3102@gmail.com>
2020-07-30 16:07:29 -0500

    Create google sign in methods

--- app/src/main/java/com/android/uptc/tgspuzzleproject/ui/Login.kt
+++ app/src/main/java/com/android/uptc/tgspuzzleproject/ui/Login.kt
@@ -1,12 +1,18 @@
 package com.android.uptc.tgspuzzleproject.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.android.uptc.tgspuzzleproject.R
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_main.*
 
 class Login : AppCompatActivity() {
 
@@ -19,6 +25,50 @@ class Login : AppCompatActivity() {
         initComponents()
     }
 
+    private fun googleSignIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    signIn()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    /*when(task.exception) {
+                        is FirebaseAuthUserCollisionException ->
+                            //toast(R.string.account_exists_message)
+                        else -> {} //main_layout.snackbar(R.string.authentication_failed)
+                    }*/
+                }
+            }
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            /*try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+            }*/
+        }
+    }
+
+    private fun signIn() {
+        //startActivity(Intent(this, HomeActivity::class.java))
+        finish()
+    }
+
     private fun initComponents() {
         // Configure Google Sign In
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -26,5 +76,11 @@ class Login : AppCompatActivity() {
             .requestEmail()
             .build()
         googleSignInClient = GoogleSignIn.getClient(this, gso)
+        google_login.setOnClickListener { googleSignIn() }
+    }
+
+    companion object {
+        private const val TAG = "GoogleActivity"
+        private const val RC_SIGN_IN = 9001
     }
 }
