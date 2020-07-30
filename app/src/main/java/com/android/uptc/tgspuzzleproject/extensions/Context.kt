3d1d1232dec90052a109a3068c44b049cbaa2b2c
jimmyale3102 <jimmyale3102@gmail.com>
2020-07-30 16:11:10 -0500

    Create extension function

---
+++ app/src/main/java/com/android/uptc/tgspuzzleproject/extensions/Context.kt
@@ -0,0 +1,12 @@
+package com.android.uptc.tgspuzzleproject.extensions
+
+import android.view.View
+import androidx.annotation.StringRes
+import com.google.android.material.snackbar.Snackbar
+
+class Context {
+
+    fun View.snack(@StringRes message: Int, duration: Int = Snackbar.LENGTH_LONG) {
+        Snackbar.make(this, message, duration).show()
+    }
+}
