9233ed28ea760df799f14104630dd35d94b176fd
jimmyale3102 <jimmyale3102@gmail.com>
2020-07-30 15:02:51 -0500

    Refactor main activity

--- app/src/main/java/com/android/uptc/tgspuzzleproject/ui/Login.kt
+++ app/src/main/java/com/android/uptc/tgspuzzleproject/ui/Login.kt
@@ -1,9 +1,10 @@
-package com.android.uptc.tgspuzzleproject
+package com.android.uptc.tgspuzzleproject.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.uptc.tgspuzzleproject.R
 
-class MainActivity : AppCompatActivity() {
+class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
