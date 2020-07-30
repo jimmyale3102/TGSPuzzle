1988f8ee5b22a3f31215faea0fbade97c07a8b40
jimmyale3102 <jimmyale3102@gmail.com>
2020-07-30 15:12:02 -0500

    Create google sign in options

--- app/src/main/java/com/android/uptc/tgspuzzleproject/ui/Login.kt
+++ app/src/main/java/com/android/uptc/tgspuzzleproject/ui/Login.kt
@@ -3,11 +3,28 @@ package com.android.uptc.tgspuzzleproject.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.android.uptc.tgspuzzleproject.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
+
+    private val auth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initComponents()
+    }
+
+    private fun initComponents() {
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
     }
 }
