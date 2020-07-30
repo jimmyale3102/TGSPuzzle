225b5fda875455f46d28719c66611f77440886af
jimmyale3102 <jimmyale3102@gmail.com>
2020-07-30 16:49:26 -0500

    Set input errors in register ac

--- app/src/main/java/com/android/uptc/tgspuzzleproject/ui/RegisterActivity.kt
+++ app/src/main/java/com/android/uptc/tgspuzzleproject/ui/RegisterActivity.kt
@@ -1,13 +1,15 @@
 package com.android.uptc.tgspuzzleproject.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import com.android.uptc.tgspuzzleproject.R
 import com.android.uptc.tgspuzzleproject.extensions.snack
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthUserCollisionException
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_register.*
 import kotlinx.android.synthetic.main.activity_register.login_button
 import kotlinx.android.synthetic.main.activity_register.password
@@ -65,6 +67,47 @@ class RegisterActivity : AppCompatActivity() {
                 }
                 register_layout.snack(R.string.complete_information_message)
             }
+        }
+
+        username.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {}
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(sequence: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if(sequence!!.toString().isNotEmpty()) {
+                    username_parent.error = null
+                }
+            }
+        })
+        password.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {}
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(sequence: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if(sequence!!.toString().isNotEmpty()) {
+                    password_parent.error = null
+                }
+            }
+        })
+        reply_password.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {}
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(sequence: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if(sequence!!.toString().isNotEmpty()) {
+                    reply_password_parent.error = null
+                }
+            }
+        })
+        login_button.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java).apply{
+                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+            })
+            finish()
         }
     }
 }
