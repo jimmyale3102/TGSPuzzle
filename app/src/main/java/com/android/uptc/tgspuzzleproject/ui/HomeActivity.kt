82cb292443c2aff5db3808c084656d613f0f58a0
jimmyale3102 <jimmyale3102@gmail.com>
2020-07-30 18:52:05 -0500

    Show home toolbar

--- app/src/main/java/com/android/uptc/tgspuzzleproject/ui/HomeActivity.kt
+++ app/src/main/java/com/android/uptc/tgspuzzleproject/ui/HomeActivity.kt
@@ -18,6 +18,7 @@ class HomeActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        setSupportActionBar(home_toolbar)
         initComponents()
     }
 
