79579aa9bbace51dbeef4ba59204452da9e22596
jimmyale3102 <jimmyale3102@gmail.com>
2020-08-15 20:25:05 -0500

    Create search word activity

---
+++ app/src/main/java/com/android/uptc/tgspuzzleproject/ui/SearchWordActivity.kt
@@ -0,0 +1,12 @@
+package com.android.uptc.tgspuzzleproject.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.uptc.tgspuzzleproject.R
+
+class SearchWordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_word)
+    }
+}
