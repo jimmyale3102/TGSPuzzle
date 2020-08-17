f9ccfd35d021635acb3d9b4b2fdfa9abc7f5dc7e
jimmyale3102 <jimmyale3102@gmail.com>
2020-08-17 14:13:46 -0500

    Create scores adapter

---
+++ app/src/main/java/com/android/uptc/tgspuzzleproject/logic/ScoresAdapter.kt
@@ -0,0 +1,30 @@
+package com.android.uptc.tgspuzzleproject.logic
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.android.uptc.tgspuzzleproject.R
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.item_scores.view.*
+
+
+class ScoresAdapter(
+    private val context: Context,
+    private val scores: ArrayList<Player>): RecyclerView.Adapter<ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(context)
+                .inflate(R.layout.item_scores, parent, false))
+    }
+
+    override fun getItemCount(): Int = scores.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val scoreData = scores[position]
+        holder.itemView.username.text = scoreData.username
+        holder.itemView.score.text = scoreData.score.toString()
+    }
+
+
+}
