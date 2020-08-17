87ba7376ec4f211578e330065054b37e903298cd
jimmyale3102 <jimmyale3102@gmail.com>
2020-08-17 13:45:10 -0500

    Improve scores layout

--- app/src/main/java/com/android/uptc/tgspuzzleproject/ui/ScoresActivity.kt
+++ app/src/main/java/com/android/uptc/tgspuzzleproject/ui/ScoresActivity.kt
@@ -3,8 +3,13 @@ package com.android.uptc.tgspuzzleproject.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.android.uptc.tgspuzzleproject.R
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ScoresActivity : AppCompatActivity() {
+
+    private val databasaeInstance by lazy { FirebaseFirestore.getInstance() }
+    //private val adapter = GroupAdapter<RecyclerView.ViewHolder>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scores)
